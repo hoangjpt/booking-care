e0d1f4432a70d9b621653ab23e3bb95a23a64b48
Quang Hoang <hoang.tq@tjtech.tech>
2022-09-29 15:56:24 +0700

    25 CRUD with nodejs

--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getHomePage } from "../controllers/homeController"
+import homeController from "../controllers/homeController"
 
 let router = express.Router()
 
 let initWebRoutes = async (app) => {
-    router.get('/', getHomePage)
+    router.get('/', homeController.getHomePage)
+    router.get('/crud', homeController.getCRUD)
+
+    router.post('/post-crud', homeController.postCRUD)
 
     return app.use("/", router)
 }
