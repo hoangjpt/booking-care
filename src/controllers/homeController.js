f2485df82a3bd46065828b93fb234eb8bcab026b
Quang Hoang <hoang.tq@tjtech.tech>
2022-10-04 14:53:58 +0700

    27 CRUD part 3

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -30,9 +30,34 @@ let displayGetCRUD = async (req, res) => {
     })
 }
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id
+    console.log(userId)
+    if(userId) {
+        let userData = await CRUDService.getUserInfoById(userId)
+        console.log(userData)
+        return res.render('editCRUD', {
+            user: userData
+        })
+    } else {
+        return res.send('User not found')
+    }
+    
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body
+    let allUsers = await CRUDService.updateUserData(data)
+    return res.render('displayCRUD', {
+        dataTable: allUsers
+    })
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
