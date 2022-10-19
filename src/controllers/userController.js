7521aec5dc8d6f5c7608bb380b20d567d8338e87
Quang Hoang <hoang.tq@tjtech.tech>
2022-10-19 14:41:51 +0700

    37 CRUD with Reactjs part 1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,6 +20,28 @@ let handleLogin = async (req, res) => {
     })
 }
 
+
+
+let handleGetAllUsers = async (req, res) => {
+    let id = req.body.type //All, id
+
+    if(!id) {
+        return res.status(200).json({
+            errCode: 0,
+            errMessage: 'Missing required parameters',
+            users: []
+        })
+    }
+    let users = await userService.getAllUsers(id)
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users
+    })
+}
+
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
 }
