f0837c88db8298586081a3986c1a32f25c22e63e
Quang Hoang <hoang.tq@tjtech.tech>
2022-10-19 16:05:58 +0700

    38 CRUD with reactjs part 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,10 +20,8 @@ let handleLogin = async (req, res) => {
     })
 }
 
-
-
 let handleGetAllUsers = async (req, res) => {
-    let id = req.body.type //All, id
+    let id = req.query.id //All, id
 
     if(!id) {
         return res.status(200).json({
